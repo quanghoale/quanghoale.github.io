5ff82f1e16916df46e157e6e35c8d3bd7e4fa58b
LE Quang Hoa <hoalq.pfiev@gmail.com>
2019-02-16 20:16:05 +0100

    Updates

---
+++ precache-manifest.a207381917a017988587375c3a6be275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8e928f41606ec09e8abc",
+    "url": "/static/js/main.8e928f41.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "8e928f41606ec09e8abc",
+    "url": "/static/css/main.235c67f0.chunk.css"
+  },
+  {
+    "revision": "efda88ac092aa7b84f83d032e490d611",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quanghoale/quanghoale.github.io/precache-manifest.e8c2330ca39e3d801202552feed3ea07.js"
+  "/precache-manifest.a207381917a017988587375c3a6be275.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quanghoale/quanghoale.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
